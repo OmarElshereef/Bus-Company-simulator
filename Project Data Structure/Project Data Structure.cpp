06c030c194408cbcd283bc3ebca9a84bdd479f41
LIGHT1079 <129695129+LIGHT1079@users.noreply.github.com>
2023-11-20 21:29:15 +0200

    Add project files.

---
+++ Project Data Structure/Project Data Structure.cpp
@@ -0,0 +1,11 @@
+// Project Data Structure.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<< "Hello World!\n";
+}
+
