69430ce209cad951f809ca6d5694fa4ed71cfc2b
LIGHT1079 <129695129+LIGHT1079@users.noreply.github.com>
2023-11-21 22:01:55 +0200

    class passenger updated

--- Project Data Structure/Passenger.h
+++ Project Data Structure/Passenger.h
@@ -8,19 +8,38 @@ using namespace std;
 class Passenger
 {
 	int arrivetime;
-	int  StartStation, EndStation;
-	int PassengerType;
+
+	int Bus_GetOnTime;
+
+	int  StartStation,CurrentStation ,EndStation;
+	
+	int PassengerPriority; //0 for normal , 1 for pregnant women, 2 for POD,3 for Aged 
+	
 	int PassengerId;
-	string PassType;
+	
+	string PassengerType; //NP for normal passenger,SP for special passenger , WP for wheel chair passenger
 
 public:
-	Passenger(string passtype = "NP");
 	
-
-
-	int getPassengerID();
+	Passenger(string passtype = "NP", int PassID = 0, int Start = 0, int End=0, string PriorityType = "no priority");
 	
+	void SetPassengerType(string Type);
+
+	void SetPassengerPriority(string type);
+
+	void UpgradePriority();
+
 	void setPassengerID(int id);
+
+    int getPassengerID();
+
+	int GetStartStation();
+
+	int GetEndStation();
+
+	int GetCurrentStation(); 
+
+	int GetPassengerPriority();
 	
 	~Passenger();
 	
