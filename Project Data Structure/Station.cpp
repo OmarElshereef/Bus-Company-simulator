615dee7f871594b664310d171f55e12bbfcc5ffe
LIGHT1079 <129695129+LIGHT1079@users.noreply.github.com>
2023-11-23 16:06:36 +0200

    added queues in stations

--- Project Data Structure/Station.cpp
+++ Project Data Structure/Station.cpp
@@ -1,1 +1,9 @@
 #include "Station.h"
+
+Station::Station()
+{
+}
+
+Station::~Station()
+{
+}
--- Project Data Structure/Station.h
+++ Project Data Structure/Station.h
@@ -1,5 +1,20 @@
 #pragma once
+#include"Bus.h"
+#include"Passenger.h"
+#include"fifoqueue.h"
+
 class Station
 {
+	fifoqueue<Bus> BusInStation;
+	fifoqueue<Passenger> NormalPassengerQ;
+	fifoqueue<Passenger> AgedPassengerQ;
+	fifoqueue<Passenger> PODPassengerQ;
+	fifoqueue<Passenger> PregnantWomenQ;
+	fifoqueue<Passenger> WheelChairQ;
+
+public:
+	Station();
+
+	~Station(); 
 };
 
